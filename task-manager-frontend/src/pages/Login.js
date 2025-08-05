84ed67c23ed9697518c2f247983585525472d141
jatin-singh-kushwaha <164042889+jatin-singh-kushwaha@users.noreply.github.com>
2025-08-06 01:41:10 +0530

    Update Login.js

--- task-manager-frontend/src/pages/Login.js
+++ task-manager-frontend/src/pages/Login.js
@@ -20,7 +20,7 @@ export default function Login() {
     e.preventDefault();
     setError('');
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', form);
+     const res = await axios.post('https://task-manager-vaeh.onrender.com/api/auth/login', form);
       const { token, user } = res.data;
       login(token, user);
       navigate('/');
